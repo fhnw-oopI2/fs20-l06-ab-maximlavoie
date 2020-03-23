152317d2acab55b32c0054feffc55cc28c6c7978
Maxim Lavoie <maxim.lavoie@students.fhnw.ch>
2020-03-23 21:57:42 +0100

    AB02 gelöst

---
+++ src/main/java/ch/fhnw/module06/ab2/ApplicationUI.java
@@ -0,0 +1,41 @@
+package ch.fhnw.module06.ab2;
+
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class ApplicationUI extends VBox {
+
+	private Label label;
+	private TextField textfield;
+	private TextArea textarea;
+	private Button button;
+
+	public ApplicationUI() {
+		initializeControls();
+		layoutControls();
+
+	}
+
+	private void initializeControls() {
+
+		label = new Label("ein Label");
+		textfield = new TextField("text field");
+		textarea = new TextArea("text area");
+		button = new Button("ein Button");
+	}
+
+	private void layoutControls() {
+		this.getChildren().addAll(label, textfield, textarea, button);
+
+		setMargin(label, new Insets(10, 10, 0, 10));
+		setMargin(textfield, new Insets(0, 10, 0, 10));
+		setMargin(textarea, new Insets(0, 10, 0, 10));
+		setMargin(button, new Insets(0, 10, 10, 10));
+
+	}
+}
